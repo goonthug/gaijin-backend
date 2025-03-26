20d68f5e6ec6040cd40bdc13997c0778536a2c5b
stud <stud@it224-15.almetpt.local>
2025-03-26 11:58:40 +0300

    Initial commit

---
+++ src/main/java/com/gaijinent/gaijin_backend/GaijinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gaijinent.gaijin_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaijinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaijinBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaijinent/gaijin_backend/GaijinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaijinent.gaijin_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaijinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
